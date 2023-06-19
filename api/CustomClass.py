13d471f8ab7ed795611e0f1fd8aa2a75c6977ae1
Hemant Sharma <hemant.kularia@gmail.com>
2023-06-19 22:56:19 +0530

    Create CustomClass.py

---
+++ api/CustomClass.py
@@ -0,0 +1,6 @@
+class CustomClass:
+    def process_url(self, url):
+        # Perform custom processing with the URL
+        # Replace this with your actual processing logic
+        result = f"Processing URL: {url}"
+        return result
